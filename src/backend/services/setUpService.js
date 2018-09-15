05be161015926d89605cdbb549ae1e41543829df
Alfie Arcede <jebjeb83@gmail.com>
2018-09-15 16:47:40 +0800

    modified setup services

--- src/backend/services/setUpService.js
+++ src/backend/services/setUpService.js
@@ -4,8 +4,8 @@ export default function(db, url, collection, beforeHook = {}, afterHook = {}) {
     return function createPlugin() {
         const app = this;
         // app.use(url, feathersMongo({ Model: db.collection(collection) }));
-        app.use(url, feathersMongo({ Model: db.collection(collection) }));
-        
+        const getDB = db.db('hackathon')
+        app.use(url, feathersMongo({ Model: getDB.collection(collection) }));
         app.service(url).hooks({ before: beforeHook, after: afterHook });
       };
 }
