28b103af1e2e673be5ec504b3cc07ca94a5fb5bc
Godfrey Zubiaga <godfreyzubiaga@gmail.com>
2018-09-16 09:02:30 +0800

    fix typo

--- src/backend/services/crop.js
+++ src/backend/services/crop.js
@@ -1,9 +1,9 @@
-import setupService from './setupService';
+import setUpService from './setUpService';
 
 function setupCropService(db) {
     const beforeHook = {};
     const afterHook = {};
-    return setupService(db, '/crops', 'crops', beforeHook, afterHook);
+    return setUpService(db, '/crops', 'crops', beforeHook, afterHook);
   }
   
   export default setupCropService;
